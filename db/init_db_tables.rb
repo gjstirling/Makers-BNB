ddc1cc4e41c66f0fa707d84535149cd8778c7e22
Graeme Stirling <graemejstirling@gmail.com>
2021-08-24 15:57:47 +0100

    Created tables for db's

--- db/init_db_tables.rb
+++ db/init_db_tables.rb
@@ -1,23 +1,37 @@
 class CreateTables
 
     def self.if_not_exists(connection)
+
+      # User table 
       connection.exec("
         CREATE TABLE IF NOT EXISTS users(
         id SERIAL PRIMARY KEY,
         name VARCHAR(100) NOT NULL,
         created_at TIMESTAMP default current_timestamp,
-        updated_at TIMESTAMP default current_timestamp
         )
       ")
-  
+      # Space table 
       connection.exec("
-        CREATE TABLE IF NOT EXISTS peeps(
+        CREATE TABLE IF NOT EXISTS spaces(
         id SERIAL PRIMARY KEY,
-        foreign_key_user_id INT,
-        foreign_key_username VARCHAR(30) NOT NULL,
-        peep VARCHAR(50) NOT NULL,
+        user_id INT NOT NULL,
+        name VARCHAR(100) NOT NULL,
+        price INT NOT NULL,
+        description VARCHAR(500) NOT NULL,
+        capacity INT NOT NULL,
         created_at TIMESTAMP default current_timestamp,
-        updated_at TIMESTAMP default current_timestamp
+        )
+      ")
+
+      # Booking table 
+      connection.exec("
+        CREATE TABLE IF NOT EXISTS bookings(
+        id SERIAL PRIMARY KEY,
+        user_id INT NOT NULL,
+        space_id INT NOT NULL,
+        start_date INT NOT NULL, 
+        end_date INT NOT NULL,
+        created_at TIMESTAMP default current_timestamp,
         )
       ")
     end
