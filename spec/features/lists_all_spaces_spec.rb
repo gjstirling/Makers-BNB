0e74ddcba8d06a8c1b32f50af80aeadb8e4b4c5a
Hotu Nga Morehu Elkington <hotunz@gmail.com>
2021-08-24 17:00:04 +0100

    Putting view space button inside the listing

--- spec/features/lists_all_spaces_spec.rb
+++ spec/features/lists_all_spaces_spec.rb
@@ -2,7 +2,7 @@ feature 'lists the spaces' do
 
   scenario 'When on the index route, spaces are listed for users to view' do
     visit('/bnb/listings')
-    expect(page).to have_content('Space 1')
+    expect(page).to have_content('Listings')
     expect(page).to have_button('View space')
     expect(page).to have_button('List space')
   end
