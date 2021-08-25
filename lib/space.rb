e87ac299cc935399154c16b8e516a663de674227
Graeme Stirling <graemejstirling@gmail.com>
2021-08-25 12:14:17 +0100

    Creating first test for space class and is now passing

--- lib/space.rb
+++ lib/space.rb
@@ -1,10 +1,14 @@
 
 class Space
 
-  attr_reader :name
+  attr_reader :name, :description, :price, :max_guests, :owner_id
 
-  def initialize(name)
+  def initialize(name, description, price, max_guests, owner_id)
     @name = name
+    @description = description
+    @price = price
+    @max_guests = max_guests
+    @owner_id = owner_id
   end
 
 end
--- spec/unit/space_spec.rb
+++ spec/unit/space_spec.rb
@@ -1,7 +1,10 @@
-
 describe Space do
-  it 'should have a name' do
-    space = Space.new('Bamboo Hut')
+  it 'should have name, description, price, max_guests, owner_id' do
+    space = Space.new('Bamboo Hut', 'A modern clean space', 90, 1, 3)
     expect(space.name).to eq('Bamboo Hut')
+    expect(space.description).to eq('A modern clean space')
+    expect(space.price).to eq(90)
+    expect(space.max_guests).to eq(1)
+    expect(space.owner_id).to eq(3)
   end
 end
