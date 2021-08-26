447a9edcf7b86f43c5e8745bd442d69bbf9a7555
Graeme Stirling <graemejstirling@gmail.com>
2021-08-26 11:33:15 +0100

    Added user/owner id test and attributes

--- lib/booking.rb
+++ lib/booking.rb
@@ -1,9 +1,11 @@
 class Booking
 
-  attr_reader :start_date, :end_date
+  attr_reader :start_date, :end_date, :user_id, :space_id
 
-  def initialize(start_date, end_date)
+  def initialize(start_date, end_date, user_id, space_id)
     @start_date = start_date
     @end_date = end_date
+    @user_id = user_id
+    @space_id = space_id
   end 
 end 
